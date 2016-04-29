2240e0851887e09d637ab77e86e8db6c1000e301
Max Holder <mxhold@gmail.com>
2016-04-29 11:00:50 -0700

    Extract methods

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,30 @@ fn main() {
 
     let mut buf = BufStream::new(stream);
 
+    let input = read_from_stdin();
+
+    write_to_buf(&mut buf, input.as_bytes());
+
+    let response = read_line_from_buf(&mut buf);
+
+    print!("{}", response);
+}
+
+fn read_from_stdin() -> String {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    let request = input.as_bytes();
+    input
+}
 
-    buf.write(request).unwrap();
+fn write_to_buf(buf: &mut BufStream<TcpStream>, bytes: &[u8]) {
+    buf.write(bytes).unwrap();
     buf.flush().unwrap();
+}
+
+fn read_line_from_buf(buf: &mut BufStream<TcpStream>) -> String {
     let mut response = String::new();
     buf.read_line(&mut response).unwrap();
-
-    print!("{}", response);
+    
+    response
 }
